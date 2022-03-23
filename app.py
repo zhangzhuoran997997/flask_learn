651033c62d48e103182f91ae49ad964eb9c8922b
zhangzhuoran997997 <63936820+zhangzhuoran997997@users.noreply.github.com>
2022-03-23 12:07:44 +0800

    minimal home page

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from glob import escape
+from flask import Flask,escape,url_for
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Welcome to My Watchlist!'
+# view function 请求处理函数 => app.route()
+# 作用为绑定上对应的URL，当在浏览器访问这个URL时就会触发这个函数
+# 可以绑定多个url地址
+@app.route('/home')
+@app.route('/zzr/<name>')
+def hello(name="user"):
+    return '<h1>Hello %s!</h1>' % escape(name)
+# url_for 函数可以得到域名地址，同时可以输入域名中需要的参数
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('hello',name="zzr"))
+    print(url_for('hello',name="zzr",number=111))
+    return "Test Page"
