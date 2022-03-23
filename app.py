9b15246a784c294a411801264ab2535cfb70552f
zhangzhuoran997997 <63936820+zhangzhuoran997997@users.noreply.github.com>
2022-03-23 23:13:18 +0800

    "Add template and static files"

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from glob import escape
-from flask import Flask,escape,url_for
+from flask import Flask,escape,url_for,render_template
+from data import name,movies 
+
 app = Flask(__name__)
+
 # view function 请求处理函数 => app.route()
 # 作用为绑定上对应的URL，当在浏览器访问这个URL时就会触发这个函数
 # 可以绑定多个url地址
@@ -15,3 +18,11 @@ def test_url_for():
     print(url_for('hello',name="zzr"))
     print(url_for('hello',name="zzr",number=111))
     return "Test Page"
+
+# template 动态文件中
+# {{...}} 用来标记变量
+# {%...%} 用来标记语句
+# {#...#} 用来写注释 
+@app.route('/')
+def index():
+    return render_template("index.html",name=name,movies=movies)
